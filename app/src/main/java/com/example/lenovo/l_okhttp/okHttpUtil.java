c78c27fa2e6a203630b2da465a81c39db9df5aeb
jccjd <1340944538@qq.com>
2018-04-30 21:11:48 +0800

    ss

--- app/src/main/java/com/example/lenovo/l_okhttp/okHttpUtil.java
+++ app/src/main/java/com/example/lenovo/l_okhttp/okHttpUtil.java
@@ -13,8 +13,15 @@ public class okHttpUtil {
     public static void requestPost(Callback callback) {
         String BASE_URL = "http://10.0.2.2/post.php";
         OkHttpClient okHttpClient = new OkHttpClient();
-        FormBody formBody = new FormBody.Builder().add("key", "Post").build();
-        Request request = new Request.Builder().url(BASE_URL).post(formBody).build();
+        FormBody formBody = new FormBody
+                .Builder()
+                .add("key", "Post")
+                .build();
+        Request request = new Request
+                .Builder()
+                .url(BASE_URL)
+                .post(formBody)
+                .build();
         Call call = okHttpClient.newCall(request);
         call.enqueue(callback);
     }
